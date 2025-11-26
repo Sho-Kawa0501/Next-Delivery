12d72eb5413031ddb7a54e75394ed92ff7067d25
Sho-Kawa0501 <akirasho0501@gmail.com>
2025-11-26 12:12:50 +0900

    不要な改行削除

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -4,8 +4,6 @@ import { Button } from "@/components/ui/button"
 export default function LoginPage() {
   return (
     <form>
-    
-      
       {/* <button formAction={signup}>Sign up</button> */}
       <Button formAction={login} variant="outline">Login Button</Button>
     </form>
